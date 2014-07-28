0ccacb47e30eebf233a4b5d87a96b71cebec9c96
github username <github email address>
2014-07-28 10:11:32 -0400

    Init Commit. CorkBoard background

---
+++ FlickrSearch/TMTViewController.m
@@ -0,0 +1,51 @@
+//
+//  TMTViewController.m
+//  FlickrSearch
+//
+//  Created by Troy Barrett on 7/28/14.
+//  Copyright (c) 2014 TMT. All rights reserved.
+//
+
+#import "TMTViewController.h"
+
+@interface TMTViewController () <UITextFieldDelegate>
+
+@property(nonatomic, weak) IBOutlet UIToolbar *toolbar;
+@property(nonatomic, weak) IBOutlet UIBarButtonItem *shareButton;
+@property(nonatomic, weak) IBOutlet UITextField *textField;
+- (IBAction)shareButtonTapped:(id)sender;
+
+@end
+
+@implementation TMTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork.png"]];
+    
+    UIImage *navBarImage = [[UIImage imageNamed:@"navbar.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(27, 27, 27, 27)];
+    [self.toolbar setBackgroundImage:navBarImage forToolbarPosition:UIToolbarPositionAny
+                          barMetrics:UIBarMetricsDefault];
+    
+    UIImage *shareButtonImage = [[UIImage imageNamed:@"button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(8, 8, 8, 8)];
+    [self.shareButton setBackgroundImage:shareButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    UIImage *textFieldImage = [[UIImage imageNamed:@"search_field.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    [self.textField setBackground:textFieldImage];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)shareButtonTapped:(id)sender {
+    // TODO
+}
+
+@end
---
+++ FlickrSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlickrSearch
+//
+//  Created by Troy Barrett on 7/28/14.
+//  Copyright (c) 2014 TMT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMTAppDelegate class]));
+    }
+}
